ec51067a76ad2f3385f02812c8a922d6f40d83c9
NatalieUsenko <usenkonatalie@gmail.com>
2017-12-05 16:57:19 +0200

    fb view controller

--- resources/views/fbposts.blade.php
+++ resources/views/fbposts.blade.php
@@ -6,9 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>FB login</title>
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #999;
+            font-family: Helvetica, Arial, sans-serif;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .one-news{
+            width: 33%;
+            padding: 0 25px 35px;
+        }
+    </style>
 </head>
 <body>
-<h3> <?php echo $userName;?></h3>
-<?php var_dump($userPosts)?>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <h3> <?php echo $userName;?></h3>
+        <?php foreach ($userPosts as $userPost) {?>
+        <div class="one-news flex-center">
+            <div class="date">Date: <?php echo $userPost['created_time']->getDate('d.m.Y');?></div>
+        </div>
+        <?php }?>
+    </div>
+</div>
+
+
+
 </body>
 </html>
