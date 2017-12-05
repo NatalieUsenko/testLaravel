f590541199981aa34133e87207f4d85d9eab3602
NatalieUsenko <usenkonatalie@gmail.com>
2017-12-05 16:24:03 +0200

    fb view controller

--- app/Http/Controllers/FbLoginController.php
+++ app/Http/Controllers/FbLoginController.php
@@ -40,7 +40,7 @@ class FbLoginController extends Controller
             try {
                 // Returns a `Facebook\FacebookResponse` object
                 $response = $fb->get('/me?fields=id,name', $accessToken);
-                $posts = $fb->get('/me/feed?fields=id,message&amp;limit=5', $accessToken);
+                $posts = $fb->get('/me/feed', $accessToken);
             } catch(\Facebook\Exceptions\FacebookResponseException $e) {
                 echo 'Graph returned an error: ' . $e->getMessage();
                 exit;
@@ -51,6 +51,7 @@ class FbLoginController extends Controller
             $user = $response->getGraphUser();
             echo $user->getName();
 
+            var_dump($posts);
             $feedEdge = $posts->getGraphEdge();
             foreach ($feedEdge as $status) {
                 var_dump($status->asArray());
